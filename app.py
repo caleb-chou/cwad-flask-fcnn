5f86b63efe5458242ee609bcde1886130c824daf
caleb <haloisawesome412@gmail.com>
2020-01-16 21:27:25 -0600

    started cv app

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/go',methods=['POST'])
+def go():
+   return redirect('/',302)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port='5000',debug=False)
